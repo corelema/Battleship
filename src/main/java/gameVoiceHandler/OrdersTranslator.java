bdcc0ef86913d217f43ea273004fbf322541d194
Corentin Leman <corentinl@slalom.com>
2016-01-25 09:09:01 -0600

    Updated intents and voice scheme, including creating and linking the methods to handle those. Added hit or miss enum. Added multiple speeches.

--- src/main/java/gameVoiceHandler/OrdersTranslator.java
+++ src/main/java/gameVoiceHandler/OrdersTranslator.java
@@ -1,29 +1,66 @@
 package gameVoiceHandler;
 
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.slu.Slot;
 import com.amazon.speech.speechlet.SpeechletResponse;
 
 /**
  * Created by corentinl on 1/23/16.
  */
 public class OrdersTranslator {
+    private static final String GRID_SIZE_SLOT = "gridSize";
+    private static final String NUMBER_OF_SHIPS_SLOT = "numberOfShips";
+    private static final String HIT_OR_MISS_SLOT = "hitOrMiss";
+    private static final String LINE_SLOT = "line";
+    private static final String COLUMN_SLOT = "column";
+    private static final String LINE_OR_COLUMN_SLOT = "lineOrColumn";
+
     public static SpeechletResponse handleFirstEventRequest() {
         return null;
     }
 
     public static SpeechletResponse handleQuickGameAsked() {
+        if (!StateManager.getInstance().isGamesStarted()) {
+            String speechOutput = String.format(Speeches.QUICK_GAME_LAUNCH, 3, 3);
+
+            String repromptText = Speeches.YOUR_TURN;
+
+            return SpeechesGenerator.newAskResponse(speechOutput, false, repromptText, false);
+        } else {
+            return null;
+        }
+    }
+
+    public static SpeechletResponse handleAdvancedGameAsked() {
         return null;
     }
 
-    public static SpeechletResponse handleParameteredGameAsked(int size, int numberOfShips) {
+    public static SpeechletResponse handleAdvancedGameAsked(Intent intent) {
+        Slot gridSizeSlot = intent.getSlot(GRID_SIZE_SLOT);
+        Slot numberOfShipsSlot = intent.getSlot(NUMBER_OF_SHIPS_SLOT);
+
         return null;
     }
 
-    public static SpeechletResponse handleFireCoordinatesGiven(int x, int y) {
+    public static SpeechletResponse handleParameterGiven(Intent intent) {
         return null;
     }
 
-    public static SpeechletResponse handleFireResultGivent(boolean isHit) {
+    public static SpeechletResponse handleFireCoordinatesGiven(Intent intent) {
         return null;
+    }
+
+    public static SpeechletResponse handleFireResultGivent(Intent intent) {
+        Slot hitOrMissSlotSlot = intent.getSlot(HIT_OR_MISS_SLOT);
+        if (hitOrMissSlotSlot != null && hitOrMissSlotSlot.getValue() != null) {
+            String speechOutput = "You said that you " + hitOrMissSlotSlot.getValue();
+
+            String repromptText = "Sorry, my abilities are limited for now.";
+
+            return SpeechesGenerator.newAskResponse(speechOutput, false, repromptText, false);
+        } else {
+            return handleHelpAsked();
+        }
     }
 
     public static SpeechletResponse handleHelpAsked() {
@@ -34,6 +71,8 @@ public class OrdersTranslator {
 
             return SpeechesGenerator.newAskResponse(speechOutput, false, repromptText, false);
         } else {
+            //TODO: determine the current state and give appropriate help.
+            //TODO: and help wasn't asked but result in a call from another handler, give an error message. Maybe the whole error process should be handled in a different method or class
             return null;
         }
     }
